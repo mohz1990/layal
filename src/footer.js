0b1839c5ee6cc65fdc5849291b3f6edea178020e
mohz1990 <m.zhr90@icloud.com>
2025-05-15 11:17:13 +0300

    footer.js

---
+++ src/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 text-center text-sm text-gray-500 py-4 mt-8">
+      جميع الحقوق محفوظة &copy; 2025 متجر ليال
+    </footer>
+  );
+}
